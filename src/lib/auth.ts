05eed99d894fbe049eb8fced000d6aabc72a258a
Prajwol Maharjan <praj.mhj11@gmail.com>
2023-04-06 21:23:17 +0545

    Fix Create and Revoke API key

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -34,6 +34,7 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async session({ token, session }) {
       if (token && session.user) {
+        session.user.id = token.id,
         session.user.name = token.name;
         session.user.email = token.email;
         session.user.image = token.picture;
